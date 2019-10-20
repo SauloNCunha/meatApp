5709877bcd72ade8e021130b3acfebd435b87649
SauloNCunha <saulonacimentocunha@hotmnail.com>
2019-10-20 13:03:04 -0300

    siscronizando

--- src/main/java/br/com/meatapp/MeatAppApplication.java
+++ src/main/java/br/com/meatapp/MeatAppApplication.java
@@ -1,51 +1,15 @@
 package br.com.meatapp;
  
-import java.util.Arrays;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.meatapp.domain.MenuItem;
-import br.com.meatapp.domain.Restaurant;
-import br.com.meatapp.domain.User;
-import br.com.meatapp.repository.ItemMenuRepository;
-import br.com.meatapp.repository.RestaurantRepository;
-import br.com.meatapp.repository.UserRepository; 
+ 
 
 @SpringBootApplication
-public class MeatAppApplication implements CommandLineRunner{
+public class MeatAppApplication{
 	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private RestaurantRepository restaurantRepository;
-	
-	@Autowired
-	private ItemMenuRepository menuItemRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(MeatAppApplication.class, args);
 	}
-	
-	@Override
-	public void run(String... args) throws Exception{
-		User user1 = new User(1, "Saulo", "saulon@hot.com", "123");
-		User user2 = new User(2, "Saulo", "saulo@gmail.com", "456");
-		userRepository.saveAll(Arrays.asList(user1, user2));
-		
-		Restaurant res1 = new Restaurant(1,"teste1","pizzaria","1:00", 1.2,"dfghj","8:00","about");
-		Restaurant res2 = new Restaurant(2,"teste1","lanchonete","2:00", 1.5,"ddasd","5:00","about1");
-		restaurantRepository.saveAll(Arrays.asList(res1,res2));
-		
-			
-		MenuItem item1 = new MenuItem(1,"item1", "teste1", 1.4, "fghfgdsgfd", res1);
-		MenuItem item2 = new MenuItem(2,"item2", "teste2", 1.7, "fghvsdvgfgdsgfd", res2);
-		menuItemRepository.saveAll(Arrays.asList(item1,item2));
-		
-	}
-
 }
